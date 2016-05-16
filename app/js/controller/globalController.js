393b61f6834c058406567ac13f27698b6b2156d8
shamaz-app <shamaz228@gmail.com>
2016-05-16 12:35:37 +0300

    now services works

--- app/js/controller/globalController.js
+++ app/js/controller/globalController.js
@@ -7,27 +7,18 @@
 
 app.controller('globalController', ['$scope', '$http', 'globalService', function ($scope, $http, globalService) {
 
-    var http = $http.get(serverUrl + '/city').success(function (data, status) {
+    globalService.getCities().success(function (data, status) {
         $scope.cities = data;
     });
 
-
     $scope.refreshMuseums = function (cityId) {
-        if (cityId !== undefined) {
-            $http.get(serverUrl + '/museum/cityId/' + cityId).success(function (data, status) {
-                $scope.museums = data;
-            });
-        } else {
-            $http.get(serverUrl + '/museum').success(function (data, status) {
-                $scope.museums = data;
-            });
-        }
+        globalService.getMuseums(cityId).success(function (data, status) {
+            $scope.museums = data;
+        });
     }
 
-    var http = $http.get(serverUrl + '/museum').success(function (data, status) {
+    $scope.refreshMuseums();
 
-        $scope.museums = data;
-    });
     $scope.selectedMuseum = {};
 
     $scope.changeCity = function () {
--- app/js/service/globalService.js
+++ app/js/service/globalService.js
@@ -6,25 +6,25 @@
 'use strict';
 
 services
-    .factory('globalService', ['$http', function($http){
+    .factory('globalService', ['$http', function ($http) {
 
         return {
-            getCities: function() {
-                return $http.get(serverUrl + '/city')
-                    .success(function(data, status) {
-                        return data;
-                    })
+            getCities: function () {
+                return $http.get(serverUrl + '/city').success(function (data, status) {
+                    return data;
+                })
             },
 
-            getMuseums: function(cityId) {
+            getMuseums: function (cityId) {
                 if (cityId !== undefined) {
-                    return $http.get(serverUrl + '/museum/', cityId).success(function (data, status) {
+                    return $http.get(serverUrl + '/museum/cityId/' + cityId).success(function (data, status) {
                         return data;
-                    })
+                    });
+                } else {
+                    return $http.get(serverUrl + '/museum').success(function (data, status) {
+                        return data;
+                    });
                 }
-                return $http.get(serverUrl + '/museum').success(function(data, status) {
-                        return data;
-                    })
             }
         };
     }]);
